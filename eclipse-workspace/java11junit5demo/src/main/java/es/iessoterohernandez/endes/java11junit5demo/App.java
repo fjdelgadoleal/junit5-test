d7bc2a85d00870ba34849ce41b8de49b7d1cdb12
Javi <fjdelgadoleal@yahoo.es>
2021-02-16 09:52:44 +0100

    first local commit

---
+++ eclipse-workspace/java11junit5demo/src/main/java/es/iessoterohernandez/endes/java11junit5demo/App.java
@@ -0,0 +1,9 @@
+package es.iessoterohernandez.endes.java11junit5demo;
+
+public class App {
+    
+    public static void main(String[] args) {
+    	System.out.println("hello world, this is a test demo class");
+    }
+    
+}
---
+++ eclipse-workspace/java11junit5demo/src/test/java/es/iessoterohernandez/endes/java11junit5demo/AppTest.java
@@ -0,0 +1,13 @@
+package es.iessoterohernandez.endes.java11junit5demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
